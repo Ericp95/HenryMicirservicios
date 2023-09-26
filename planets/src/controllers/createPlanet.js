1a7e67298c40f69364673ae0480c0da4d19e4d79
Ericp95 <ericp6960@gmail.com>
2023-09-26 16:27:54 -0300

    arreglo req body en createPlanet

--- planets/src/controllers/createPlanet.js
+++ planets/src/controllers/createPlanet.js
@@ -3,7 +3,7 @@ const data=require("../data");
 module.exports=async (req,res)=>{
 
     
-    const createPlanet= await data.createPlanet();
+    const createPlanet= await data.createPlanet(req.body);
     
     res.status(200).json(createPlanet)
 }
