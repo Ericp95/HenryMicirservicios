b2a3d21ad922fcc7ba96bf9cd1b75bfa06ab88e5
Ericp95 <ericp6960@gmail.com>
2023-09-26 16:26:58 -0300

    arreglo req body en createFilm

--- films/src/controllers/createFilm.js
+++ films/src/controllers/createFilm.js
@@ -4,7 +4,7 @@ const data=require("../data")
 module.exports= async(req,res)=>{
 
     
-    const createFilm= await data.createFilm()
+    const createFilm= await data.createFilm(req.body)
     
     res.status(200).json(createFilm)
 } 
