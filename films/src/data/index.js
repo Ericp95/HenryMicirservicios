64814bf9decf14b32c3abb70cc7d98f89f73cdc1
Ericp95 <ericp6960@gmail.com>
2023-09-26 15:20:26 -0300

    creando data film

---
+++ films/src/data/index.js
@@ -0,0 +1,23 @@
+const axios=require("axios");
+
+
+module.exports={
+
+    list: async () => {//trae todos los character
+       const film=await axios.get("http://database:8004/Film")
+       return film.data 
+
+      },
+
+
+      filmID: async (id) => {//trae por id character
+        const filmId = await axios.get(`http://database:8004/Film/${id}`);
+        return filmId.data;
+      },
+
+      createFilm: async (film) => {
+        
+        const createNewFilm = await axios.post("http://database:8004/Film",film);
+        return createNewFilm.data;
+      },
+}
