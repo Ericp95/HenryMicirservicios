707f2244d981d73fff17466b418c06733d11e30f
Ericp95 <ericp6960@gmail.com>
2023-09-26 16:22:16 -0300

    arreglo body controllers character

--- characters/src/controllers/createCharacter.js
+++ characters/src/controllers/createCharacter.js
@@ -3,7 +3,7 @@ const data = require("../data");
 module.exports=async(req,res)=>{
 
     
-    const createCharac= await data.createCharacter()
+    const createCharac= await data.createCharacter(req.body)
     //const characterIdResp= characterId.data
 
     res.status(200).json(createCharac);
