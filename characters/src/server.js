0b32b210a8ed5ac784c97cd70efdf36462a374a9
Ericp95 <ericp6960@gmail.com>
2023-09-27 10:14:22 -0300

    poniendo manejador errores en server

--- characters/src/server.js
+++ characters/src/server.js
@@ -9,5 +9,17 @@ server.use(express.json());
 
 server.use(require("./routes"));
 
+server.use("*",(req,res)=>{//sino encuentra ningun endpoint da el 404
+    res.status(404).send("not found")
+})
+
+server.use((err,req,res,next)=>{
+   // res.send("este es el manejador de errores")
+   res.status(err.statusCode || 500).send({
+    error:true,
+    message:err.message
+   });
+})//arreglarlo que no andaa!!
+
 
 module.exports=server;
