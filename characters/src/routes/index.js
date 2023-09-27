1662490a99f247193cf5e9fd8d18b7122f1b7c12
Ericp95 <ericp6960@gmail.com>
2023-09-27 10:11:40 -0300

    poniendo middleware controlar errores routes characters

--- characters/src/routes/index.js
+++ characters/src/routes/index.js
@@ -1,13 +1,14 @@
 const {Router}=require("express");
 const controllers = require("../controllers");
+const middleware= require("../middleware");
 
 const router=Router();
 
-router.get("/characters", controllers.getCharacter);
+router.get("/characters", middleware.characterValidation,controllers.getCharacter);
 
-router.get("/characters/:id", controllers.getCharacterId);
+router.get("/characters/:id",middleware.characterValidation ,controllers.getCharacterId);
 
-router.post("/characters", controllers.createCharacter);
+router.post("/characters", middleware.characterValidation,controllers.createCharacter);
 
 
 module.exports=router;
