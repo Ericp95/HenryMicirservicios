cefc00b35b51416fe075baedbd45ff139f850888
Ericp95 <ericp6960@gmail.com>
2023-09-27 09:59:37 -0300

    colocando response error en createCharacter

--- characters/src/controllers/createCharacter.js
+++ characters/src/controllers/createCharacter.js
@@ -1,4 +1,5 @@
 const data = require("../data");
+const response=require("../utils");
 
 module.exports=async(req,res)=>{
 
@@ -6,5 +7,6 @@ module.exports=async(req,res)=>{
     const createCharac= await data.createCharacter(req.body)
     //const characterIdResp= characterId.data
 
-    res.status(200).json(createCharac);
+    //res.status(200).json(createCharac);
+    response(res,200,createCharac);
 }     
