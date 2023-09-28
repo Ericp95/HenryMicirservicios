bac337d27c932c67c8dbe58698e2e5c572ec0dda
Ericp95 <ericp6960@gmail.com>
2023-09-28 09:49:46 -0300

    agregando response en funcion getPlanetId

--- planets/src/controllers/getPlanetId.js
+++ planets/src/controllers/getPlanetId.js
@@ -1,9 +1,11 @@
-const data=require("../data")
+const data=require("../data");
+const {response}=require("../utils");
 
 module.exports=async (req,res)=>{
 
     const{id}=req.params
     const planetId= await data.planetID(id);
     
-    res.status(200).json(planetId)
+    //res.status(200).json(planetId)
+    response(res,200,planetId);
 }
