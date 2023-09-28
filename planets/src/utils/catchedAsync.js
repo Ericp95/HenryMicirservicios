4396101090c4bf20b8567aaa3fa0a8b97f03922c
Ericp95 <ericp6960@gmail.com>
2023-09-28 09:38:38 -0300

    creando carpeta utils y sus componentes

---
+++ planets/src/utils/catchedAsync.js
@@ -0,0 +1,14 @@
+module.exports= (fn)=>{  // con eñ fn recibe la funcion create personaje
+
+    return (req,res,next)=>{
+         fn(req,res).catch((err)=>{//controlador de la funcion osea fn necesita req y res y ejecuta la funcion y si hay error lo lleva al .catch //con el .catch agarramos el error de la promesa
+             next(err);// si hay error saltemos al manejador de errores de express
+         })                                    
+                                              
+    }
+};
+
+
+//esto se usa para el manejo de errores y para no estar usando simepre el trycatch en todos lados!!
+
+//esto se llama funcion del orden superior 
---
+++ planets/src/utils/index.js
@@ -0,0 +1,6 @@
+module.exports={
+
+    errors:require("./errors"),
+    response:require("./response"),
+    catchedAsync:require("./catchedAsync"),
+} 
