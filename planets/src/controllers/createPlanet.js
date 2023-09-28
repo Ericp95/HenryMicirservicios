059625c3835125c11977524d689181834963f0d4
Ericp95 <ericp6960@gmail.com>
2023-09-28 09:45:53 -0300

    agrego response en la funcion createPlanets

--- planets/src/controllers/createPlanet.js
+++ planets/src/controllers/createPlanet.js
@@ -1,9 +1,12 @@
 const data=require("../data");
+const {response}=require("../utils");
 
 module.exports=async (req,res)=>{
 
     
     const createPlanet= await data.createPlanet(req.body);
     
-    res.status(200).json(createPlanet)
+   // res.status(200).json(createPlanet)
+
+   response(res,200,createPlanet)
 }
