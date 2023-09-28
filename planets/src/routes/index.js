848d3c0df63cbf5c5870c86f99b0905854de06a0
Ericp95 <ericp6960@gmail.com>
2023-09-28 09:51:47 -0300

    agregando validador en la ruta post

--- planets/src/routes/index.js
+++ planets/src/routes/index.js
@@ -1,12 +1,14 @@
 const {Router}=require("express");
 const controllers = require("../controllers");
 
+const middleware=require("../middleware");
+
 const router=Router();
 
 router.get("/planets", controllers.getPlanet);
 
 router.get("/planets/:id", controllers.getPlanetId);
 
-router.post("/planets", controllers.createPlanet);
+router.post("/planets", middleware.planetValidator, controllers.createPlanet);
 
 module.exports=router;
