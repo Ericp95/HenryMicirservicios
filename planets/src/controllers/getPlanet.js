b319a086454afabaefb956396c75d1a9b3cef6b0
Ericp95 <ericp6960@gmail.com>
2023-09-28 09:47:45 -0300

    agrego response funcion getPlanet

--- planets/src/controllers/getPlanet.js
+++ planets/src/controllers/getPlanet.js
@@ -1,8 +1,10 @@
-const data=require("../data")
+const data=require("../data");
+const {response}=require("../utils");
 
 module.exports=async(req,res)=>{
 
     const planets= await data.list()
     
-    res.status(200).json(planets)
+    //res.status(200).json(planets)
+    response(res,200,planets)
 };
